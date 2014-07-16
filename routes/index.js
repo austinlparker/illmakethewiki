fb4e4a982baf6363970738cf86e0fd98ba6d0ec2
Austin Parker <austinlparker@gmail.com>
2014-07-16 18:59:11 -0400

    testing

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@
 exports.index = function(req, res){
 	console.log(req.subdomains);
 	var sub = req.subdomains[0];
+	console.log(sub);
   res.render('index', { title: "I'll Make The Wiki",
   						subdomain: sub });
 };
